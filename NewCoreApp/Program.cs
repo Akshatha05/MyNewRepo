2c425cf22325ab764271185f65847a5ef42508ba
Akshatha <Akshatha@DESKTOP-0CFPG9A>
2020-11-25 16:19:02 +0530

    test

--- NewCoreApp/Program.cs
+++ NewCoreApp/Program.cs
@@ -6,7 +6,7 @@ namespace NewCoreApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!!!");
         }
     }
 }
